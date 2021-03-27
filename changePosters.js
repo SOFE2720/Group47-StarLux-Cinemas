ba904c94992cffcd828c0b97a271f8e819681070
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2021-03-27 16:41:22 -0400

    test change

--- changePosters.js
+++ changePosters.js
@@ -2,7 +2,7 @@ function change_posters(){
     var location = document.getElementById('theatre').value; //gets theatre location name
     var postersBtn = document.getElementsByName("selectedMovie"); //gets poster buttons
     var posters = document.getElementsByName("posters"); //gets movie posters
-
+    //change test
     //IMDB ids of movies offered in brampton location
     var brampton = ['tt7846844', 'tt4566758', 'tt7329656', 'tt4154796', 'tt0369610', 'tt1431045', 'tt6105098', 'tt1323594', 'tt8946378'];
     //poster urls of movies offered in brampton location
