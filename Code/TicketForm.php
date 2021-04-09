b8d9a82d1bb6a4db8bbc724ee63a187604ccdb26
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2021-04-09 19:58:23 -0400

    ticket booking from complete

--- Code/TicketForm.php
+++ Code/TicketForm.php
@@ -33,70 +33,70 @@
                     <div class= "movietype">Standard Tickets</div>
                     <div class= "ticketTypeSelection">
                         <h3 class="ticketType">General Admission</h3>
-                        <p class="price">$14.99 x <input type ="number" min ="1" id = "generalS" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                        <p class="price">$14.99 x <input type ="number" min ="1" id = "generalS" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                     </div>
                     
                     <div class = "ticketTypeSelection">
                         <h3 class="ticketType">Senior (65+)</h3>
-                        <p class="price">$9.99 x <input type ="number" min ="1" id = "seniorS" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                        <p class="price">$9.99 x <input type ="number" min ="1" id = "seniorS" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                     </div>
 
                     <div class = "ticketTypeSelection">
                         <h3 class="ticketType">Children(3-13)</h3>
-                        <p class="price">$8.99 x <input type ="number" min ="1" id = "childS" class = "counter" oninput="total()"oninput="validity.valid||(value='');"> </p>
+                        <p class="price">$8.99 x <input type ="number" min ="1" id = "childS" class = "counter" oninput="total(false)"oninput="validity.valid||(value='');"> </p>
                     </div>
                     
                     <div class= "movietype">3D Tickets</div>
                     <div class = "ticketTypeSelection">
                         <h3 class="ticketType">General Admission</h3>
-                        <p class="price">$19.99 x <input type ="number" min ="1" id = "general3" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                        <p class="price">$19.99 x <input type ="number" min ="1" id = "general3" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                     </div>
 
                     <div class = "ticketTypeSelection">
                         <h3 class="ticketType">Senior (65+)</h3>
-                        <p class="price">$14.99 x <input type ="number" min ="1" id = "senior3" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                        <p class="price">$14.99 x <input type ="number" min ="1" id = "senior3" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                     </div>
                     
                     <div class = "ticketTypeSelection">
                         <h3 class="ticketType">Children(3-13)</h3>
-                        <p class="price">$13.99 x <input type ="number" min ="1" id = "child3" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                        <p class="price">$13.99 x <input type ="number" min ="1" id = "child3" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                     </div>
                     
                     <div class= "movietype">IMAX Tickets</div>
                     <div class = "ticketTypeSelection">
                         <h3 class="ticketType">General Admission</h3>
-                        <p class="price">$24.99 x <input type ="number" min ="1" id = "generalI" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                        <p class="price">$24.99 x <input type ="number" min ="1" id = "generalI" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                     </div>
 
                     <div class = "ticketTypeSelection">
                         <h3 class="ticketType">Senior (65+)</h3>
-                        <p class="price">$19.99 x <input type ="number" min ="1" id = "seniorI" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                        <p class="price">$19.99 x <input type ="number" min ="1" id = "seniorI" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                     </div>
                     
                     <div class = "ticketTypeSelection">
                         <h3 class="ticketType">Children(3-13)</h3>
-                        <p class="price">$18.99 x <input type ="number" min ="1" id = "childI" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                        <p class="price">$18.99 x <input type ="number" min ="1" id = "childI" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                     </div>
                     <?php if ($location == 'Markham' or $location == 'Brampton'){?>
                         <div class= "movietype">D-BOX Tickets</div>
                         <div class = "ticketTypeSelection">
                             <h3 class="ticketType">Children(3-13)</h3>
-                            <p class="price">$18.99 x <input type ="number" min ="1" id = "childI" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                            <p class="price">$18.99 x <input type ="number" min ="1" id = "childI" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                         </div>
 
                         <div class = "ticketTypeSelection">
                             <h3 class="ticketType">General Admission</h3>
-                            <p class="price">$29.99 x <input type ="number" min ="1" id = "generalD" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p> 
+                            <p class="price">$29.99 x <input type ="number" min ="1" id = "generalD" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p> 
                         </div>
                 
                         <div class = "ticketTypeSelection">
                             <h3 class="ticketType">Senior (65+)</h3>
-                            <p class="price">$24.99 x <input type ="number" min ="1" id = "seniorD" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                            <p class="price">$24.99 x <input type ="number" min ="1" id = "seniorD" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                         </div>            
                             
                         <div class = "ticketTypeSelection">
                             <h3 class="ticketType">Children(3-13)</h3>
-                            <p class="price">$23.99 x <input type ="number" min ="1" id = "childD" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                            <p class="price">$23.99 x <input type ="number" min ="1" id = "childD" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                         </div>
                     <?php } ?>
                     
@@ -104,17 +104,17 @@
                         <div class= "movietype">ULTRA AVX Tickets</div>
                         <div class = "ticketTypeSelection">
                             <h3 class="ticketType">General Admission</h3>
-                            <p class="price">$32.99 x <input type ="number" min ="1" id = "generalAX" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                            <p class="price">$32.99 x <input type ="number" min ="1" id = "generalAX" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                         </div>
                         
                         <div class = "ticketTypeSelection">
                             <h3 class="ticketType">Senior (65+)</h3>
-                            <p class="price">$27.99 x <input type ="number" min ="1" id = "seniorAX" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                            <p class="price">$27.99 x <input type ="number" min ="1" id = "seniorAX" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                         </div>
                 
                         <div class = "ticketTypeSelection">
                             <h3 class="ticketType">Children(3-13)</h3>
-                            <p class="price">$26.99 x <input type ="number" min ="1" id = "childAX" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                            <p class="price">$26.99 x <input type ="number" min ="1" id = "childAX" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                         </div>
                     <?php } ?>
                     
@@ -122,29 +122,30 @@
                         <div class= "movietype">ATMOS Tickets</div>
                         <div class = "ticketTypeSelection">
                             <h3 class="Addmission">General Admission</h3>
-                            <p class="price">$34.99 x <input type ="number" min ="1" id = "generalA" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                            <p class="price">$34.99 x <input type ="number" min ="1" id = "generalA" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                         </div>
                 
                         <div class = "ticketTypeSelection">
                             <h3 class="Senior">Senior (65+)</h3>
-                            <p class="price">$29.99 x <input type ="number" min ="1" id = "seniorA" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                            <p class="price">$29.99 x <input type ="number" min ="1" id = "seniorA" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                         </div>
                         
                         <div class = "ticketTypeSelection">
                             <h3 class="child">Children(3-13)</h3>
-                            <p class="price">$28.99 x <input type ="number" min ="1" id = "childA" class = "counter" oninput="total()" oninput="validity.valid||(value='');"></p>
+                            <p class="price">$28.99 x <input type ="number" min ="1" id = "childA" class = "counter" oninput="total(false)" oninput="validity.valid||(value='');"></p>
                         </div>
                     <?php } ?>
                 </div> 
                 <div id = "displayTotals">
                     <div id = "order" class = "title">Order Summary</div> <br>
                     <br>
-                    <div id= "subtotal" class = "printTotal">Sutotal: </div> <br>
+                    <div id= "subtotal"false class = "printTotal">Subtotal: </div> <br>
                     <div id = "tax" class = "printTotal">Tax: </div> <br>
-                    <div id= "total" class = "printTotal">Total: </div>
+                    <div id= "total"false class = "printTotal">Total: </div>
                     <br>
-                    <button type="submit" id="subbtn" value="submit">Purchase Tickets</button>
+                    <button type="submit" id="subbtn" value="submit" onclick="total(true)">Purchase Tickets</button>
                 </div>
+                <br><br><br>
             </div>
         </form>
     </body>
--- Code/TicketsCalculations.js
+++ Code/TicketsCalculations.js
@@ -1,54 +1,65 @@
 //Calculates the price that is to be paid for the tickets
 
 var a = JSON.parse(localStorage.getItem('selectedSeats'));
+var disableBtn = false;
 
-function total(){
+function total(submit){
     var theatreLocation = document.getElementById('theatreLocation').value;
-    price = (getNumOfTickets('generalS') * 14.99) + (getNumOfTickets('seniorS') * 9.99) + (getNumOfTickets('childS') * 8.99) + (getNumOfTickets('general3') * 19.99) + (getNumOfTickets('senior3') * 14.99) + (getNumOfTickets('child3') * 13.99) + (getNumOfTickets('generalI') * 24.99) + (getNumOfTickets('seniorI') * 19.99) + (getNumOfTickets('childI') * 18.99);
-    
+    var price = (getNumOfTickets('generalS') * 14.99) + (getNumOfTickets('seniorS') * 9.99) + (getNumOfTickets('childS') * 8.99) + (getNumOfTickets('general3') * 19.99) + (getNumOfTickets('senior3') * 14.99) + (getNumOfTickets('child3') * 13.99) + (getNumOfTickets('generalI') * 24.99) + (getNumOfTickets('seniorI') * 19.99) + (getNumOfTickets('childI') * 18.99);
+    var numOfTickets = getNumOfTickets('generalS') + getNumOfTickets('seniorS') + getNumOfTickets('childS') + getNumOfTickets('general3') + getNumOfTickets('senior3') + getNumOfTickets('child3') + getNumOfTickets('generalI') + getNumOfTickets('seniorI') + getNumOfTickets('childI');
+
     if (theatreLocation == 'Markham' || theatreLocation == 'Brampton'){
         price += (getNumOfTickets('generalD') * 29.99) + (getNumOfTickets('seniorD') * 24.99) + (getNumOfTickets('childD') * 23.99);
+        numOfTickets += getNumOfTickets('generalD') + getNumOfTickets('seniorD') + getNumOfTickets('childD');
+
     }
     if (theatreLocation == 'North York'){
         price += (getNumOfTickets('generalA') * 32.99) + (getNumOfTickets('seniorA') * 27.99) + (getNumOfTickets('childA') * 26.99);
+        numOfTickets += getNumOfTickets('generalA') + getNumOfTickets('seniorA') + getNumOfTickets('childA');
+
     }
     if (theatreLocation != 'North York'){
         price += (getNumOfTickets('generalAX') * 34.99) + (getNumOfTickets('seniorAX') * 29.99) + (getNumOfTickets('childAX') * 28.99);
+        numOfTickets += getNumOfTickets('generalAX') + getNumOfTickets('seniorAX') + getNumOfTickets('childAX');
+
     }
 
-    // if (getNumOfTickets('generalS') + getNumOfTickets('seniorS') + getNumOfTickets('childS') + getNumOfTickets('general3') + getNumOfTickets('senior3') + getNumOfTickets('child3') + getNumOfTickets('generalI') + getNumOfTickets('seniorI')  + getNumOfTickets('childI') + getNumOfTickets('generalD') + getNumOfTickets('seniorD') + getNumOfTickets('childD') + getNumOfTickets('generalA') + getNumOfTickets('seniorA') + getNumOfTickets('childA') + getNumOfTickets('generalAX') + getNumOfTickets('seniorAX') + getNumOfTickets('childAX') > a.length){
-    //     alert ("You must choose exactly " + a.length + " ticket(s)");
-    // }
-    // else{
-    //     var ST = document.getElementById("subtotal");
-    //     ST.innerHTML = "$" + price.toFixed(2);
+    if (numOfTickets > a.length){
+        alert ("You must choose exactly " + a.length + " ticket(s)");
+        console.log('invalid');
+        if (!disableBtn){
+            document.getElementById("subbtn").disabled = true;
+            disableBtn = true; 
+        }
+    }
+    else{
+        console.log('valid');
+        console.log(disableBtn);
+        if (disableBtn){
+            document.getElementById("subbtn").disabled = false;
+            disableBtn = false;
+        }
+        var ST = document.getElementById("subtotal");
+        ST.innerHTML = "Subtotal: $" + price.toFixed(2);
 
-    //     var tax = price * 0.13;
-    //     var T = document.getElementById("tax");
-    //     T.innerHTML = "$" + tax.toFixed(2);
+        var tax = price * 0.13;
+        var T = document.getElementById("tax");
+        T.innerHTML = "Tax: $" + tax.toFixed(2);
 
-    //     var total = price * 1.13;
-    //     var t = document.getElementById("total");
-    //     t.innerHTML = "$" + total.toFixed(2);
-
-    //     localStorage.clear();
-    // }
-    console.log(price);
+        var total = price * 1.13;
+        var t = document.getElementById("total");
+        t.innerHTML = "Total: $" + total.toFixed(2);
+    }
+    if (submit){
+        if (numOfTickets < a.length){
+            alert ("You must choose exactly " + a.length + " ticket(s)");
+            if (!disableBtn){
+                document.getElementById("subbtn").disabled = true;
+                disableBtn = true; 
+            }
+        }
+    }
 }
-
-
-// function Thanks(){
-//     var total = document.getElementById('total').innerText;
-//     //check if any inputs where made, if not then display a alert messsage
-//     if (total == ""){
-//         alert ("You didn't make a selection");
-//     }
-//     else{
-//         var msg = document.getElementById("thanks");
-//         msg.innerHTML = "Thank You for Purchasing Tickets from StarLux Cinemas. Enjoy Your Show!";
-//     }
-// }
-
 
 function getNumOfTickets(idName){
     var SG = document.forms["ticketform"].elements[idName];
