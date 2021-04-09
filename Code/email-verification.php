0d9b857c981c98df7aada9a3227b4e449ab4348f
Foram <Foramgandhi12@yahoo.ca>
2021-04-09 02:12:25 -0400

    create email verification page

---
+++ Code/email-verification.php
@@ -0,0 +1,29 @@
+<?php
+include 'checkUser.php';
+include 'TicketForm.php';
+session_start();
+
+$database = mysqli_connect('localhost', 'root', '', 'webfinal');
+
+if(isset($_POST['submit'])){
+    $date = $_POST['date'];
+    $location = $_POST['theatres'];
+    $movie_name = $_POST['movies'];
+}
+
+$email = $_SESSION['email'];
+
+// message
+$msg = "Thank You for Choosing StarLux Cinemas.\n\nThis is a purchase confirmation 
+for your order.\n\nMovie: <b>$movie_name</b>\nDate: <b>$date</b>\nTheatre Location: 
+    <b>$location</b>\n\nEnjoy the show!";
+
+$msg = wordwrap($msg, 70);
+
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    // sends email
+    mail($email, "StarLux e-Ticket Purchase Confirmation", $msg);
+}
+
+include 'verification-page'; 
+?>
