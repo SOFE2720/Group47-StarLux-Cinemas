9c7a225dc62e3f904fde0c6494d8a9b574b0e3b6
Foram <Foramgandhi12@yahoo.ca>
2021-04-09 02:15:36 -0400

    create email verification page

---
+++ Code/verification-page.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Email Verification Page</title>
+    <!--attach css files-->
+    <link rel="stylesheet" href="css/navBar.css" type="text/css">
+    <link rel="stylesheet" href="css/verify.css" type="text/css">
+</head>
+<body>
+    <!--StarLux Cinemas logo-->
+    <img src="images/MainLogo.png" id="main"><br><br>
+    <!--navigation bar-->
+    <div class="topnav">
+        <a href="main-page.html" id="home">Home</a>
+        <a href="Theatres Page.html">Theatres</a>
+        <a href="Movie_Experiences.html">Movie Experiences</a>
+        <a href="foods_and_drinks.html">Food and Drinks</a>
+        <a href="LSFormT.php">Book Tickets</a>
+    </div><br><br><br>
+
+    <?php include 'email-verification.php';?>
+
+    <div>
+        <hr><h1>Thank You!</h1>
+        <h2>Your booking was completed successfully.</h2><br>
+
+        <img id="email" src="images/email.png" />
+
+        <p id="text">An email receipt including the details of your booking has been<br> 
+            sent to the email address provided. Please keep it for your records.
+        </p>
+    </div>
+    <br><br><hr><br>
+
+    <a class="link" href="main-page.html">Return to Main Page</a>
+    
+    <br><br><br>
+    <p id="copyright">&#169 StarLux Cinemas.</p>
+
+</body>
+</html>
